60d46edf1765b109e3905e27be30112a989fb7a5
Everaldo Lima <jemlima@gmail.com>
2019-10-09 11:45:05 -0300

    Feat: Notificações em realtime.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
+
+const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+mongoose.connect("mongodb://localhost:27017/semana09", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-app.listen(3333);
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(routes);
+
+server.listen(3333);
